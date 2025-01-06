54ec49ed662b0f45a879bbba8ca14ed62f71d89a
Junior Martins <amjr.box@gmail.com>
2025-01-06 00:29:46 -0300

    refactor(diarization): remove token de autenticação hardcoded
    Remove o token de autenticação hardcoded do Pipeline de diarização de fala.
    Isso melhora a segurança e flexibilidade, permitindo que o token seja fornecido
    de forma mais segura, como através de variáveis de ambiente ou configurações externas.

--- src/others/diarization.py
+++ src/others/diarization.py
@@ -1,6 +1,6 @@
 from pyannote.audio import Pipeline
 pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
-                                    use_auth_token="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")
+                                    use_auth_token="")
 
 
 # apply the pipeline to an audio file
